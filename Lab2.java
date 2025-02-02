3158449112714a587ddf49a2cf1bb6494f8af3cc
HoweyLuong <165950202+HoweyLuong@users.noreply.github.com>
2025-02-01 17:57:08 -0800

    Add files via upload

---
+++ Lab2.java
@@ -0,0 +1,154 @@
+package Calculator;
+import java.util.*;
+import java.lang.Math;
+public class Lab2 {
+	//Use hashMap to put the arithmetic for the HashMap;
+		private static final Map <String, Integer> current = new HashMap<>();
+		
+		static {
+			current.put("+",1);
+			current.put("-",1);
+			current.put("*",2);
+			current.put("/",2);
+			current.put("^",3);
+			current.put("sin",4);
+			current.put("cos",4);
+			current.put("tan",4);
+			current.put("cot",4);
+			current.put("ln",4);
+			current.put("log",4);
+			current.put("(", 0);
+			current.put(")",0);
+			current.put("{", 0);
+			current.put("}",0);
+			
+		}
+		/**
+		 * 
+		 * @param args
+		 */
+		
+		
+		public static void main (String[]args) {
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Please insert your arithemetic equation: ");
+			String number = scanner.nextLine();
+			
+			try {
+				double result = evaluateExpression(number);
+				System.out.println("Result: " + result);
+			}catch (ArithmeticException e) {
+				System.out.println(e.getMessage());
+			}
+			catch (Exception e) {
+				System.out.println("Error: " + e.getMessage());
+			}
+			
+			scanner.close();
+		}
+		
+		
+		
+		//Use the function to calculate 
+		private static double evaluateExpression(String express) {
+			Stack <Double> values = new Stack<> ();
+			Stack <String> op = new Stack<> ();
+			
+			StringTokenizer token = new StringTokenizer(express, "+-*/^(){}", true);
+			
+			while (token.hasMoreTokens()) {
+				String now = token.nextToken().trim();
+				if(now.isEmpty()) {
+					continue;
+				}
+				if(isNumeric(now)) {
+					values.push(Double.parseDouble(now));
+					
+				}
+			
+				else if(now.equals("(") || now.equals("{")) {
+					op.push(now);
+				}
+				else if (now.equals(")")) {
+					while(!op.isEmpty() && !op.peek().equals("(")) {
+						compute(values,op.pop());
+					}
+					if(!op.isEmpty() && op.peek().equals("(")) {
+						op.pop();
+					}else {
+						throw new IllegalArgumentException("Not match parantheses " + now);
+					}
+				}
+					
+					
+					else if(now.equals("}")) {
+						while(!op.isEmpty() && !op.peek().equals("{")) {
+							compute(values, op.pop());
+						}
+						if(!op.isEmpty() && op.peek().equals("{")) {
+							op.pop();
+						}else {
+						throw new IllegalArgumentException("Not match curly braces: " + now);
+					}
+				}
+					else if(current.containsKey(now)) {
+						while(!op.isEmpty() && current.get(op.peek()) >= current.get(now)
+								&& !op.peek().equals("(") && !op.peek().equals("{")) {
+							compute(values, op.pop());
+						}
+						op.push(now);
+					}
+					else {
+						throw new IllegalArgumentException("Invalid Input: " + now);
+					}
+			}
+			
+			while(!op.isEmpty()) {
+				compute(values,op.pop());
+			}
+				
+			return values.pop();
+				
+				
+			
+			
+			}
+		
+		
+		private static void compute(Stack<Double> values, String op) {
+			double b = values.pop();
+			double a = values.isEmpty() ? 0 : values.pop();
+			
+			switch(op) {
+			
+			case "+" : values.push(a+b); break;
+			case "-" : values.push(a-b); break;
+			case "*" : values.push(a*b); break;
+			case "/" : 
+				if (b == 0) {
+					throw new ArithmeticException("Error: b can not be 0 ");
+				}else {
+					values.push(a/b);
+					break;
+				}
+			case "^":
+				values.push(Math.pow(a, b));
+				break;
+				
+			default: throw new IllegalArgumentException("Unknow Operator: " + op);
+				
+				
+			}
+		}
+		
+		
+		
+		private static boolean isNumeric(String str) {
+			try {
+				Double.parseDouble(str);
+				return true;
+			} catch(NumberFormatException e) {
+				return false;
+			}
+		}
+}
